480e913a05fecf169e0addab0ef5e3fb8110704e
Roland735 <85491901+Roland735@users.noreply.github.com>
2025-01-03 11:49:20 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['firebasestorage.googleapis.com'],
     },
+    exportTrailingSlash: true, // Ensures that pages are exported with a trailing slash (required for GitHub Pages)
 };
 
 export default nextConfig;
